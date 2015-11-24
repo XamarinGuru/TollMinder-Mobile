f11732ad34c4e26af66e8473e59ad308847927d6
Max Grigoriev <themaxgrig@gmail.com>
2015-11-24 17:56:19 +0200

    Android MvvmCross

--- Tollminder.Core/App.cs
+++ Tollminder.Core/App.cs
@@ -11,7 +11,7 @@ namespace Tollminder.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 				
-            RegisterAppStart<ViewModels.FirstViewModel>();
+            RegisterAppStart<ViewModels.HomeViewModel>();
         }
     }
 }
