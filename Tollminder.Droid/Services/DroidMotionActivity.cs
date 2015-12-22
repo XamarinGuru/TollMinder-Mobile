64f332d7112ea620cf539b13663e913a80c2bf3e
Nickolas Shpotenko <kolyashpotenko@gmail.com>
2015-12-22 17:22:55 +0200

    Added new feature.

---
+++ Tollminder.Droid/Services/DroidMotionActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tollminder.Core.Services;
+using Tollminder.Core.Models;
+using Android.Content;
+using Tollminder.Droid.AndroidServices;
+using Tollminder.Droid.Handlers;
+using Tollminder.Droid.ServicesConnections;
+
+namespace Tollminder.Droid.Services
+{
+	public class DroidMotionActivity : AndroidServiceWithServiceConnection<MotionActivityService,MotionClientHanlder, MotionServiceConnection> , IMotionActivity
+	{
+		public bool AuthInProgress { get; set; } = false;
+		#region IMotionActivity implementation
+
+		public void StartDetection ()
+		{
+			Start ();	
+		}
+
+		public void StopDetection ()
+		{
+			Stop ();
+		}
+
+		public MotionType MotionType {
+			get {
+				return MotionType.Still;
+			}
+		}
+
+		#endregion
+	}
+}
+
