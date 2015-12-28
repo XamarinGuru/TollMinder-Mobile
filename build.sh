c932d5349e60d0bb86136c42ffeff7bee8afdba2
Nickolas Shpotenko <kolyashpotenko@gmail.com>
2015-12-28 17:42:14 +0200

    Improve logic.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -Version 4.4.2
+if [ ! -f /packages/FAKE.4.4.2/tools/FAKE.exe ]; then
+ mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -Version 4.4.2
+fi
 mono --runtime=v4.0 packages/FAKE.4.4.2/tools/FAKE.exe build.fsx $@
