d74e7b4dfae4a25bfb17cdbf4a26d7a83ae27c12
Nickolas Shpotenko <kolyashpotenko@gmail.com>
2016-01-12 16:48:39 +0200

    Added progress model for downloading

--- Tollminder.Core/Models/DownloadBytesProgress.cs
+++ Tollminder.Core/Models/DownloadBytesProgress.cs
@@ -4,9 +4,22 @@ namespace Tollminder.Core.Models
 {
 	public class DownloadBytesProgress
 	{
-		public DownloadBytesProgress ()
+		public DownloadBytesProgress(string fileName, int bytesReceived, int totalBytes)
 		{
+			Filename = fileName;
+			BytesReceived = bytesReceived;
+			TotalBytes = totalBytes;
 		}
+
+		public int TotalBytes { get; private set; }
+
+		public int BytesReceived { get; private set; }
+
+		public float PercentComplete { get { return ((float)BytesReceived / TotalBytes) * 100; } }
+
+		public string Filename { get; private set; }
+
+		public bool IsFinished { get { return BytesReceived == TotalBytes; } }
 	}
 }
 
