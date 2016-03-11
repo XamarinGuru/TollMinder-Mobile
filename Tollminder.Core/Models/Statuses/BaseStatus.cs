8420cf7eb2553f193731ef60e303a8f5ecec6641
Mykola Shpotenko <mshpotenko@gloriumtech.com>
2016-03-11 13:53:43 +0200

    Forgot one file to add

---
+++ Tollminder.Core/Models/Statuses/BaseStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MvvmCross.Platform;
+using Tollminder.Core.Services;
+
+namespace Tollminder.Core.Models.Statuses
+{
+	public abstract class BaseStatus
+	{
+		protected IGeoLocationWatcher GeoWatcher { get; } = Mvx.Resolve<IGeoLocationWatcher> ();
+		protected IMotionActivity MotionActivity { get; } = Mvx.Resolve<IMotionActivity> ();
+		protected IDistanceChecker DistanceChecker { get; } = Mvx.Resolve<IDistanceChecker> ();
+		protected IWaypointChecker WaypointChecker { get; } = Mvx.Resolve<IWaypointChecker> ();
+		protected IGeoDataService DataService { get; } = Mvx.Resolve<IGeoDataService> ();
+
+		public abstract TollGeolocationStatus CheckStatus ();
+	}
+}
+
