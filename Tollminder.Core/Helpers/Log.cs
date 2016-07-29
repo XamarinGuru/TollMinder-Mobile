4c550c51e9b9aed3f7518942198d53410fa85599
Mykola Shpotenko <mshpotenko@gloriumtech.com>
2016-07-29 13:53:08 +0300

    Remove static log.

--- Tollminder.Core/Helpers/Log.cs
+++ Tollminder.Core/Helpers/Log.cs
@@ -10,9 +10,9 @@ namespace Tollminder.Core.Helpers
 		public static StringBuilder _messageLog = new StringBuilder ();
 		public static void LogMessage(string message)
 		{
-			_messageLog.Append (message);
-			_messageLog.Append ("\n");
-			Mvx.Resolve<IMvxMessenger> ().Publish (new LogUpdated (_messageLog));
+			//_messageLog.Append (message);
+			//_messageLog.Append ("\n");
+			//Mvx.Resolve<IMvxMessenger> ().Publish (new LogUpdated (_messageLog));
 			Mvx.Trace (MvvmCross.Platform.Platform.MvxTraceLevel.Diagnostic, message, string.Empty);
 		}
 	}
