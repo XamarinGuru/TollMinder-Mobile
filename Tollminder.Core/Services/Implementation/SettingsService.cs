1d3e4a24b697b3c90e1d4bb5dbba9d7b0d7a45ad
Max Ishchenko <mishchenko@gloriumtech.com>
2016-10-05 12:09:22 +0300

    create SettingsService

--- Tollminder.Core/Services/Implementation/SettingsService.cs
+++ Tollminder.Core/Services/Implementation/SettingsService.cs
@@ -5,7 +5,7 @@ namespace Tollminder.Core.Services.Implementation
     {
         public static double WaypointDistanceRequired = 0.05;
         public static double DistanceToWaypointRadius = 0.3;
-        public static double WaypointAreaDistanceRequired = 0.05;
+        public static double WaypointAreaDistanceRequired = 0.08;
         public static int DistanceIntervalDefault = 10;
         public static int DistanceIntervalHighDefault = 5;
     }
