9d9073ab29ce3fb7c29e788b87c5e29eb8343e6f
Max Ishchenko <mishchenko@gloriumtech.com>
2016-10-10 18:47:43 +0300

    android SpeechToText fixes

--- Tollminder.Droid/Services/DroidSpeechToTextService.cs
+++ Tollminder.Droid/Services/DroidSpeechToTextService.cs
@@ -17,159 +17,164 @@ using Tollminder.Droid.Views;
 
 namespace Tollminder.Droid.Services
 {
-	public class DroidSpeechToTextService : Java.Lang.Object, ISpeechToTextService, IRecognitionListener
-	{
-		//public static int VoiceConstId = 911;
-		TaskCompletionSource<bool> _recognitionTask;
+    public class DroidSpeechToTextService : Java.Lang.Object, ISpeechToTextService, IRecognitionListener
+    {
+        //public static int VoiceConstId = 911;
+        TaskCompletionSource<bool> _recognitionTask;
 
-		SpeechRecognizer _speechRecognizer;
-        Intent voiceIntent;
-		Handler _handler;
-		AlertDialog _dialog;
-        bool _dialogManualAnswer;
-		bool _isMusicRunning;
-        Ringtone _ringtone;
+        SpeechRecognizer _speechRecognizer;
 
-		IPlatform _platform;
-		IPlatform Platform
-		{
-			get
-			{
-				return _platform ?? (_platform = Mvx.Resolve<IPlatform>());
-			}
-		}
+        Handler _handler;
+        AlertDialog _dialog;
+        bool _dialogWasManuallyAnswered;
+        bool _isMusicRunning;
 
-		ITextFromSpeechMappingService _mappingService;
-		ITextFromSpeechMappingService MappingService
-		{
-			get
-			{
-				return _mappingService ?? (_mappingService = Mvx.Resolve<ITextFromSpeechMappingService>());
-			}
-		}
+        IPlatform _platform;
+        IPlatform Platform
+        {
+            get
+            {
+                return _platform ?? (_platform = Mvx.Resolve<IPlatform>());
+            }
+        }
 
-		ITextToSpeechService _textToSpeechService;
-		ITextToSpeechService TextToSpeechService
-		{
-			get
-			{
-				return _textToSpeechService ?? (_textToSpeechService = Mvx.Resolve<ITextToSpeechService>());
-			}
-		}
+        ITextFromSpeechMappingService _mappingService;
+        ITextFromSpeechMappingService MappingService
+        {
+            get
+            {
+                return _mappingService ?? (_mappingService = Mvx.Resolve<ITextFromSpeechMappingService>());
+            }
+        }
 
-		string _question;
-		public string Question
-		{
-			get
-			{
-				return _question;
-			}
-			set
-			{
-				_question = value;
-			}
-		}
+        ITextToSpeechService _textToSpeechService;
+        ITextToSpeechService TextToSpeechService
+        {
+            get
+            {
+                return _textToSpeechService ?? (_textToSpeechService = Mvx.Resolve<ITextToSpeechService>());
+            }
+        }
 
-		public Task<bool> AskQuestion(string question)
-		{
-			_isMusicRunning = Platform.IsMusicRunning;
+        string _question;
+        public string Question
+        {
+            get
+            {
+                return _question;
+            }
+            set
+            {
+                _question = value;
+            }
+        }
 
-			if (_isMusicRunning)
-				Platform.PauseMusic();
-			_recognitionTask = new TaskCompletionSource<bool>();
-
-			if (Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity == null)
-			{
-				var otherActivity = new Intent(Application.Context, typeof(HomeView));
-				otherActivity.AddFlags(ActivityFlags.NewTask);
-				Application.Context.StartActivity(otherActivity);
-
-				EnsureActivityLoaded().Wait();
-			}
-
-			_handler = new Handler(Application.Context.MainLooper);
-
-			TextToSpeechService.Speak(question, false).Wait();
-
-			Question = question;
-
-			StartSpeechRecognition();
-
-			return _recognitionTask.Task;
-		}
-
-		void StartSpeechRecognition()
-		{
-            _handler.Post(() => _dialog?.Cancel());
-            _dialogManualAnswer = false;
-			_handler.Post(() =>
-				{
-					if (_dialog == null)
-					{
-						try
-						{
-                            _dialogManualAnswer = false;
-							_dialog = new AlertDialog.Builder(Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity)
-									.SetTitle(Question)
-									.SetMessage("Please, answer yes or no after the tone")
-									.SetCancelable(false)
-                                    .SetPositiveButton("Yes", (sender, e) => 
-                        {
-                            _dialogManualAnswer = true;
-                            _speechRecognizer?.StopListening();
-                            _recognitionTask.TrySetResult(true);
-                        })
-                                    .SetNegativeButton("No", (sender, e) =>
-                        {
-                            _dialogManualAnswer = true;
-                            _speechRecognizer?.StopListening();
-                            _recognitionTask.TrySetResult(false);
-                        })
-									.Show();
-						}
-						catch (Exception e)
-						{
-							Mvx.Trace(e.Message + e.StackTrace);
-						}
-					}
-					else
-					{
-						_dialog.Show();
-					}
-				});
-
-
-			voiceIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
-			voiceIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
-			voiceIntent.PutExtra(RecognizerIntent.ExtraCallingPackage, "com.tollminder");
-		    voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 5000);
-			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 5000);
-			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
-			voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
-
-			voiceIntent.PutExtra(RecognizerIntent.ExtraLanguage, "en-US");
-
-			//Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity.StartActivityForResult(voiceIntent, VoiceConstId);
-			//Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity.RunOnUiThread(() =>
-
-			_handler.Post(() =>
-		   {
-				if (_speechRecognizer == null)
-			   {
-                    TextToSpeechService.Speak("Please, answer yes or no after the tone", false).Wait();
-				   _speechRecognizer = SpeechRecognizer.CreateSpeechRecognizer(Application.Context);
-			   }
-                else
+        Intent _voiceIntent;
+        public Intent VoiceIntent
+        {
+            get
+            {
+                if (_voiceIntent == null)
                 {
-                    StopRecognizer();
-                    _speechRecognizer = SpeechRecognizer.CreateSpeechRecognizer(Application.Context);
+                    _voiceIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraCallingPackage, "com.tollminder");
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 5000);
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 5000);
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
+
+                    _voiceIntent.PutExtra(RecognizerIntent.ExtraLanguage, "en-US");
                 }
+
+                return _voiceIntent;
+            }
+        }
+
+        public Task<bool> AskQuestion(string question)
+        {
+            _isMusicRunning = Platform.IsMusicRunning;
+
+            if (_isMusicRunning)
+                Platform.PauseMusic();
+            _recognitionTask = new TaskCompletionSource<bool>();
+
+            if (Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity == null)
+            {
+                var otherActivity = new Intent(Application.Context, typeof(HomeView));
+                otherActivity.AddFlags(ActivityFlags.NewTask);
+                Application.Context.StartActivity(otherActivity);
+
+                EnsureActivityLoaded().Wait();
+            }
+
+            _handler = new Handler(Application.Context.MainLooper);
+
+            TextToSpeechService.Speak(question, false).Wait();
+
+            Question = question;
+
+            StartSpeechRecognition();
+
+            return _recognitionTask.Task;
+        }
+
+        void StartSpeechRecognition()
+        {
+            _handler.Post(() => _dialog?.Cancel());
+            _dialogWasManuallyAnswered = false;
+
+            _handler.Post(() =>
+           {
+                if (_dialog == null)
+               {
+                   try
+                   {
+                       _dialogWasManuallyAnswered = false;
+                       _dialog = new AlertDialog.Builder(Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity)
+                               .SetTitle(Question)
+                               .SetMessage("Please, answer yes or no after the tone")
+                               .SetCancelable(false)
+                               .SetPositiveButton("Yes", (sender, e) =>
+                   {
+                       _dialogWasManuallyAnswered = true;
+                       _speechRecognizer?.StopListening();
+                       _recognitionTask.TrySetResult(true);
+                   })
+                               .SetNegativeButton("No", (sender, e) =>
+                   {
+                       _dialogWasManuallyAnswered = true;
+                       _speechRecognizer?.StopListening();
+                       _recognitionTask.TrySetResult(false);
+                   })
+                               .Show();
+                   }
+                   catch (Exception e)
+                   {
+                       Mvx.Trace(e.Message + e.StackTrace);
+                   }
+               }
+               else
+               {
+                   _dialog.Show();
+               }
+
+               if (_speechRecognizer == null)
+               {
+                   TextToSpeechService.Speak("Please, answer yes or no after the tone", false).Wait();
+                   _speechRecognizer = SpeechRecognizer.CreateSpeechRecognizer(Application.Context);
+               }
+               else
+               {
+                   StopRecognizer();
+                   _speechRecognizer = SpeechRecognizer.CreateSpeechRecognizer(Application.Context);
+               }
 
                //Platform.MuteAudio();
                _speechRecognizer.SetRecognitionListener(this);
-			   _speechRecognizer.StartListening(voiceIntent);
-		   }); 
-		}
+               _speechRecognizer.StartListening(VoiceIntent);
+           });
+        }
 
         void StopRecognizer()
         {
@@ -178,37 +183,37 @@ namespace Tollminder.Droid.Services
             _speechRecognizer?.Destroy();
         }
 
-		Task<bool> EnsureActivityLoaded()
-		{
-			var _ensureTask = new TaskCompletionSource<bool>();
-			Mvx.Resolve<IMvxMessenger>().SubscribeOnThreadPoolThread<SpechRecognitionActivityLoadedMessage>(x =>
-			{
-				Console.WriteLine("Received SpechRecognitionActivityLoadedMessage");
-				_ensureTask.SetResult(true);
-			});
-			return _ensureTask.Task;
-		}
+        Task<bool> EnsureActivityLoaded()
+        {
+            var _ensureTask = new TaskCompletionSource<bool>();
+            Mvx.Resolve<IMvxMessenger>().SubscribeOnThreadPoolThread<SpechRecognitionActivityLoadedMessage>(x =>
+            {
+                Console.WriteLine("Received SpechRecognitionActivityLoadedMessage");
+                _ensureTask.SetResult(true);
+            });
+            return _ensureTask.Task;
+        }
 
-		public void OnBeginningOfSpeech()
-		{
-			Console.WriteLine("OnBeginningOfSpeech");
-		}
+        public void OnBeginningOfSpeech()
+        {
+            Console.WriteLine("OnBeginningOfSpeech");
+        }
 
-		public void OnBufferReceived(byte[] buffer)
-		{
-			Console.WriteLine("OnBufferReceived");
-		}
+        public void OnBufferReceived(byte[] buffer)
+        {
+            Console.WriteLine("OnBufferReceived");
+        }
 
-		public void OnEndOfSpeech()
-		{
-			Console.WriteLine("OnEndOfSpeech");
-		}
+        public void OnEndOfSpeech()
+        {
+            Console.WriteLine("OnEndOfSpeech");
+        }
 
-		public void OnError([GeneratedEnum] SpeechRecognizerError error)
-		{
-			Core.Helpers.Log.LogMessage("SpeechRecognizerError = " + error);
+        public void OnError([GeneratedEnum] SpeechRecognizerError error)
+        {
+            Core.Helpers.Log.LogMessage("SpeechRecognizerError = " + error);
 
-            if (!_dialogManualAnswer && error == SpeechRecognizerError.NoMatch)
+            if (!_dialogWasManuallyAnswered && error == SpeechRecognizerError.NoMatch)
             {
                 Task.Run(() =>
                 {
@@ -219,31 +224,30 @@ namespace Tollminder.Droid.Services
                     TextToSpeechService.Speak("Please, retry", false).Wait();
                     Task.Delay(1500).Wait();
                     StartSpeechRecognition();
-            });
+                });
             }
-		}
+        }
 
-		public void OnEvent(int eventType, Bundle @params)
-		{
-			Console.WriteLine("OnEvent");
-		}
+        public void OnEvent(int eventType, Bundle @params)
+        {
+            Console.WriteLine("OnEvent");
+        }
 
-		public void OnPartialResults(Bundle partialResults)
-		{
-			Console.WriteLine("OnPartialResults");
-		}
+        public void OnPartialResults(Bundle partialResults)
+        {
+            Console.WriteLine("OnPartialResults");
+        }
 
-		public void OnReadyForSpeech(Bundle @params)
-		{
-			Console.WriteLine("OnReadyForSpeech");
-		}
+        public void OnReadyForSpeech(Bundle @params)
+        {
+            Console.WriteLine("OnReadyForSpeech");
+        }
 
-		public void OnResults(Bundle results)
-		{
+        public void OnResults(Bundle results)
+        {
             Console.WriteLine("OnResults");
-            if (!_dialogManualAnswer)
+            if (!_dialogWasManuallyAnswered)
             {
-
                 var answer = MappingService.DetectAnswer(results.GetStringArrayList(SpeechRecognizer.ResultsRecognition));
 
                 _handler.Post(() =>
@@ -262,15 +266,15 @@ namespace Tollminder.Droid.Services
                 }
                 else
                 {
-    				AskQuestion(Question);
-			    }
+                    AskQuestion(Question);
+                }
             }
-		}
+        }
 
-		public void OnRmsChanged(float rmsdB)
-		{
-			Console.WriteLine("OnRmsChanged");
-		}
-	}
+        public void OnRmsChanged(float rmsdB)
+        {
+            Console.WriteLine("OnRmsChanged");
+        }
+    }
 }
 
