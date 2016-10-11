a86d74ff1333ca3a669259cfe081fbff20ebacdc
Max Ishchenko <mishchenko@gloriumtech.com>
2016-10-11 14:15:44 +0300

    update broadcast receiver (MvvmCross 4.3.0)

--- Tollminder.Droid/BroadcastReceivers/GeolocationReceiver.cs
+++ Tollminder.Droid/BroadcastReceivers/GeolocationReceiver.cs
@@ -1,46 +1,30 @@
-﻿using System;
-using System.Threading.Tasks;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Gms.Location;
 using Android.Locations;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
-using MvvmCross.Droid.Platform;
+using MvvmCross.Droid.Services;
 using MvvmCross.Platform;
-using MvvmCross.Platform.Droid.Platform;
-using MvvmCross.Plugins.Messenger;
-using Tollminder.Core.Helpers;
-using Tollminder.Core.Models;
 using Tollminder.Core.Services;
-using Tollminder.Droid.AndroidServices;
 
 namespace Tollminder.Droid.BroadcastReceivers
 {
 	[RegisterAttribute("tollminder.droid.services.DroidGeolocationWatcher")]
 	[BroadcastReceiver (Enabled = true, Exported = false)]
 	[IntentFilter (new [] { "com.tollminder.GeolocationReciever" }, Priority = (int)IntentFilterPriority.HighPriority)]
-	public class GeolocationReceiver : BroadcastReceiver
+	public class GeolocationReceiver : MvxBroadcastReceiver
 	{
 		public override void OnReceive (Context context, Intent intent)
 		{
+            base.OnReceive(context, intent);
+
 			if (LocationResult.HasResult (intent)) {
 				LocationResult locationResult = LocationResult.ExtractResult (intent);
 				Location location = locationResult.LastLocation;
-				if (location != null) {
-					try
-					{
-						var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(Application.Context ?? context);
-						setup.EnsureInitialized();
-					}
-					catch(Exception e)
-					{
-						//TODO: some mvvmcross initialization issies while deleting and starting app on thirt time
-						//Cannot start primary - as state already InitializingSecondary
-						Mvx.Trace(e.Message + e.StackTrace);
-					}
-
+				if (location != null) 
+                {
 					Mvx.Resolve<IGeoLocationWatcher>().Location = location.GetGeolocationFromAndroidLocation();
 
 					new Handler(context.MainLooper).Post(() =>
--- Tollminder.Droid/BroadcastReceivers/MotionReciever.cs
+++ Tollminder.Droid/BroadcastReceivers/MotionReciever.cs
@@ -1,37 +1,24 @@
 ﻿using MvvmCross.Platform;
-using Tollminder.Core.Models;
 using Tollminder.Core.Services;
-using Tollminder.Droid.AndroidServices;
-using MvvmCross.Plugins.Messenger;
 using Android.Content;
 using Android.Gms.Location;
 using Android.App;
-using MvvmCross.Droid.Platform;
-using System;
+using MvvmCross.Droid.Services;
 
 namespace Tollminder.Droid.BroadcastReceivers
 {
 	[BroadcastReceiver (Enabled = true, Exported = false)]
 	[IntentFilter (new [] { "com.tollminder.MotionReciever" })]
-	public class MotionReciever : BroadcastReceiver
+	public class MotionReciever : MvxBroadcastReceiver
 	{
 		public override void OnReceive (Context context, Intent intent)
 		{
+            base.OnReceive(context, intent);
+
 			if (ActivityRecognitionResult.HasResult (intent)) {
 				ActivityRecognitionResult result = ActivityRecognitionResult.ExtractResult (intent);
-				if (result != null) {
-					try
-					{
-						var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(Application.Context ?? context);
-						setup.EnsureInitialized();
-					}
-					catch (Exception e)
-					{
-						//TODO: some mvvmcross initialization issies while deleting and starting app on thirt time
-						//Cannot start primary - as state already InitializingSecondary
-						Mvx.Trace(e.Message + e.StackTrace);
-					}
-
+				if (result != null) 
+                {
 					Mvx.Resolve<IMotionActivity> ().MotionType = result.MostProbableActivity.GetMotionType ();
 				}                
 			}
