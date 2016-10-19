50ddc850770f8db918dde033d746b301c491092b
Max Ishchenko <mishchenko@gloriumtech.com>
2016-10-19 18:57:25 +0300

    changed logger

--- Tollminder.Core/Helpers/Log.cs
+++ Tollminder.Core/Helpers/Log.cs
@@ -9,13 +9,21 @@ namespace Tollminder.Core.Helpers
 	public static class Log
 	{
 		public static StringBuilder _messageLog = new StringBuilder ();
+        static int counter = 0;
 
 		public static void LogMessage(string message)
 		{
 			//#if DEBUG
 			Mvx.Trace (MvvmCross.Platform.Platform.MvxTraceLevel.Diagnostic, message, string.Empty);
 
+            if (counter > 500)
+            {
+                _messageLog.Clear();
+                counter = 0;
+            }
+
 			_messageLog.AppendLine($"[{DateTime.Now}] {message}");
+            counter++;
 			try
 			{
 				Mvx.Resolve<IMvxMessenger>()?.Publish(new LogUpdated(new object()));
