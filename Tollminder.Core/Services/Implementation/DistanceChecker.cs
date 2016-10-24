0c39ac75695486f19e5525e81ccf57df32d0c5d5
Max Ishchenko <mishchenko@gloriumtech.com>
2016-10-24 14:19:25 +0300

    refactor of distandechecker

--- Tollminder.Core/Services/Implementation/DistanceChecker.cs
+++ Tollminder.Core/Services/Implementation/DistanceChecker.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Tollminder.Core.Models;
 using System.Collections.Generic; 
-using Tollminder.Core.Helpers;
-using System.Threading.Tasks;
 using static Tollminder.Core.Helpers.LocationChecker;
 
 namespace Tollminder.Core.Services.Implementation
@@ -12,23 +9,14 @@ namespace Tollminder.Core.Services.Implementation
 	{
 		public double Epsilon { get; } = double.Epsilon;
 
-		private readonly IGeoLocationWatcher _geoWatcher;
-		private readonly IWaypointChecker _waypointChecker;
-
-		public DistanceChecker (IGeoLocationWatcher geoWatcher, IWaypointChecker waypointChecker)
-		{
-			this._geoWatcher = geoWatcher;
-			this._waypointChecker = waypointChecker;
-		}
-
         public List<TollPointWithDistance> GetMostClosestWaypoint (GeoLocation center, List<TollPoint> points, double radius = double.MaxValue)
 		{
             return points.AsParallel()
                          .AsOrdered()
                          .WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+                         .Where(x => DistanceBetweenGeoLocations(center, x.Location) - radius < Epsilon)
                          .Select(x => new TollPointWithDistance(x, DistanceBetweenGeoLocations(center, x.Location)))
                          .OrderBy(x => x.Distance)
-                         .Where(x => x.Distance - radius < Epsilon)
                          .ToList();
 		}
     }
