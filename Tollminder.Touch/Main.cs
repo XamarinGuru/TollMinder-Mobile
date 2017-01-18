0c5b76c62702b6ccd948e92982b985ac7f5577e9
Roman Sarafanov <rsarafanov@gloriumtech.com>
2017-01-18 02:01:04 +0200

    slider done, without data fields

--- Tollminder.Touch/Main.cs
+++ Tollminder.Touch/Main.cs
@@ -3,7 +3,7 @@ using UIKit;
 namespace Tollminder.Touch
 {
 	public class Application
-	{
+    {
 		static void Main (string[] args)
 		{
 			UIApplication.Main (args, null, "AppDelegate");
--- Tollminder.Touch/Views/HomeView.cs
+++ Tollminder.Touch/Views/HomeView.cs
@@ -31,6 +31,8 @@ namespace Tollminder.Touch.Views
         RoundedButton callCentergButton;
         RoundedButton logoutButton;
 
+        UIScrollView boardScrollView;
+        UIView boardContainerView;
         UIView buttonContainerView;
         UIView roadInformationContainerView;
 
@@ -52,9 +54,27 @@ namespace Tollminder.Touch.Views
         {
             base.InitializeObjects();
 
+            nfloat height = 50.0f;
+            nfloat width = 50.0f;
+            nfloat padding = 10.0f;
+            nint n = 25;
+
+            boardScrollView = new UIScrollView{
+                Frame = new CGRect(0, 100, View.Frame.Width, height + 2 * padding),
+                ContentSize = new CGSize((width + padding) * n, height),
+                AutoresizingMask = UIViewAutoresizing.FlexibleWidth
+            };
+            boardScrollView.PagingEnabled = true;
+
             var topView = new UIView();
+            boardContainerView = new UIView();
+
             buttonContainerView = new UIView();
+            buttonContainerView.Frame = new CGRect(0, 0, (boardScrollView.Bounds.Width), (boardScrollView.Bounds.Height * 3));
+
             roadInformationContainerView = new UIView();
+            roadInformationContainerView.Frame = new CGRect(340, 0, boardScrollView.Bounds.Width - 50, (boardScrollView.Bounds.Height * 2.5));
+            
             var bottomView = new UIView();
             var applicationLogo = new UIImageView(UIImage.FromBundle(@"Images/logo.png"));
             var callCenterLabel = new UILabel();
@@ -96,31 +116,50 @@ namespace Tollminder.Touch.Views
             //callCenterLabel.Text = "+(1)305 335 85 08";
             //callCenterLabel.TextColor = UIColor.LightGray;
 
-            buttonContainerView.Alpha = 1.0f;
             applicationBoard.Frame = new CoreGraphics.CGRect(10, 10, applicationBoard.Image.CGImage.Width, applicationBoard.Image.CGImage.Height);
-            buttonContainerView.AddIfNotNull(applicationBoard, profileButton, payButton, payHistoryButton);
+            buttonContainerView.AddIfNotNull(profileButton, payButton, payHistoryButton);
             buttonContainerView.AddConstraints(
-                applicationBoard.WithSameHeight(buttonContainerView),
-                applicationBoard.WithSameWidth(buttonContainerView),
-                applicationBoard.WithSameCenterX(buttonContainerView),
-                applicationBoard.WithSameCenterY(buttonContainerView),
-
                 profileButton.AtTopOf(buttonContainerView, 10),
                 profileButton.AtLeftOf(buttonContainerView, 8),
                 profileButton.WithRelativeWidth(buttonContainerView, 0.4f),
-                profileButton.WithRelativeHeight(buttonContainerView, 0.47f),
+                profileButton.WithRelativeHeight(buttonContainerView, 0.8f),
 
                 payHistoryButton.AtTopOf(buttonContainerView, 10),
                 payHistoryButton.WithSameCenterX(buttonContainerView),
                 payHistoryButton.WithRelativeWidth(buttonContainerView, 0.4f),
-                payHistoryButton.WithRelativeHeight(buttonContainerView, 0.47f),
+                payHistoryButton.WithRelativeHeight(buttonContainerView, 0.8f),
 
                 payButton.AtTopOf(buttonContainerView, 10),
                 payButton.AtRightOf(buttonContainerView, 8),
                 payButton.WithRelativeWidth(buttonContainerView, 0.4f),
-                payButton.WithRelativeHeight(buttonContainerView, 0.47f)
+                payButton.WithRelativeHeight(buttonContainerView, 0.8f)
             );
-            
+
+            // Road Information Container
+            roadInformationContainerView.BackgroundColor = UIColor.Blue;
+
+            boardScrollView.ContentSize = new CGSize((buttonContainerView.Bounds.Width + roadInformationContainerView.Bounds.Width - 40), boardScrollView.Frame.Height);
+            boardScrollView.AddIfNotNull(buttonContainerView, roadInformationContainerView);
+            boardScrollView.AddConstraints(
+                buttonContainerView.AtTopOf(boardScrollView),
+                buttonContainerView.AtLeftOf(boardScrollView),
+                buttonContainerView.WithSameHeight(boardScrollView),
+                buttonContainerView.WithRelativeWidth(boardScrollView, 1f)
+            );
+
+            boardContainerView.AddIfNotNull(applicationBoard, boardScrollView);
+            boardContainerView.AddConstraints(
+                applicationBoard.WithSameHeight(boardContainerView),
+                applicationBoard.WithSameWidth(boardContainerView),
+                applicationBoard.WithSameCenterX(boardContainerView),
+                applicationBoard.WithSameCenterY(boardContainerView),
+
+                boardScrollView.AtTopOf(boardContainerView, 10),
+                boardScrollView.AtLeftOf(boardContainerView, 8),
+                boardScrollView.AtRightOf(boardContainerView, 8),
+                boardScrollView.WithRelativeHeight(boardContainerView, 0.55f)
+            );
+         
             bottomView.AddIfNotNull(trackingButton);
             bottomView.AddConstraints(
                 trackingButton.AtTopOf(bottomView),
@@ -129,21 +168,20 @@ namespace Tollminder.Touch.Views
                 trackingButton.WithRelativeHeight(bottomView, EnvironmentInfo.GetTrackingButtonHeight),
                 trackingButton.WithRelativeWidth(bottomView, EnvironmentInfo.GetTrackingButtonWidth)
             );
-            var width = View.Frame.Width-30;
-            buttonContainerView.AddConstraint(NSLayoutConstraint.Create(buttonContainerView, NSLayoutAttribute.Width, NSLayoutRelation.Equal, 1f, width));
 
-            View.AddIfNotNull(topView, buttonContainerView, bottomView);
+            View.AddIfNotNull(topView, boardContainerView, bottomView);
             View.AddConstraints(
                 topView.AtTopOf(View),
                 topView.AtLeftOf(View),
                 topView.AtRightOf(View),
                 topView.WithRelativeHeight(View, 0.2f),
 
-                buttonContainerView.Below(topView),
-                buttonContainerView.AtLeftOf(View, 15),
-                buttonContainerView.WithRelativeHeight(View, 0.43f),
+                boardContainerView.Below(topView),
+                boardContainerView.AtLeftOf(View, 15),
+                boardContainerView.AtRightOf(View, 15),
+                boardContainerView.WithRelativeHeight(View, 0.43f),
 
-                bottomView.Below(buttonContainerView),
+                bottomView.Below(boardContainerView),
                 bottomView.WithSameCenterX(topView),
                 bottomView.WithRelativeHeight(View, 0.27f),
                 bottomView.AtBottomOf(View, 30)
